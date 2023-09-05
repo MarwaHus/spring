b512a1b7aab6b5fe6db7ff6b2add882029096668
MarwaHus <marwa.hu18@gmail.com>
2023-09-05 20:33:39 +0300

    lab11

--- src/main/java/com/songr/demo/controller/Album.java
+++ src/main/java/com/songr/demo/controller/Album.java
@@ -6,21 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+
 @Controller
 public class Album {
-
-
-
     @GetMapping("/albums")
-    public ModelAndView albums() {
-        AlbumModel[] albums = {
-                new AlbumModel("Thriller", "Michael Jackson", 9, 357, "https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png"),
-                new AlbumModel("Back in Black", "AC/DC", 10, 42*60+11, "https://upload.wikimedia.org/wikipedia/en/1/17/Acdcbackinblack.png"),
-                new AlbumModel("Rumours", "Fleetwood Mac", 11, 40*60+1, "https://upload.wikimedia.org/wikipedia/en/0/04/FleetwoodMacRumours.PNG")
-        };
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("albums");
-        modelAndView.addObject("albums", albums);
-        return modelAndView;
+    public String albums(Model m) {
+        AlbumModel a1 = new AlbumModel("Justice", "Justin Bieber", 16, 52 * 60 + 34, "https://upload.wikimedia.org/wikipedia/en/5/5c/Justice_by_Justin_Bieber.png");
+
+        AlbumModel a2 = new AlbumModel("lovely", "Billie Eilish, Khalid", 16, 56 * 60 + 17, "https://th.bing.com/th?q=Lovely+Song+Lyrics+Billie+Eilish+and+Khalid&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-XA&cc=JO&setlang=en&adlt=strict&t=1&mw=247");
+
+        AlbumModel a3 = new AlbumModel("Red (Taylor's Version)", "Taylor Swift", 30, 106 * 60 + 18, "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Red_Taylor%27s_Version.png/220px-Red_Taylor%27s_Version.png");
+
+        m.addAttribute("albums", new AlbumModel[]{a1, a2, a3});
+        return "album";
     }
-}
+    }
